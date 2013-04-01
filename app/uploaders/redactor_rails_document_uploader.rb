2d867416d084ec193764f36758751588032ffd32
Brandon Hansen <brandonh@ibethel.org>
2013-04-01 16:32:51 -0700

    Use fog to upload images

--- app/uploaders/redactor_rails_document_uploader.rb
+++ app/uploaders/redactor_rails_document_uploader.rb
@@ -3,7 +3,7 @@ class RedactorRailsDocumentUploader < CarrierWave::Uploader::Base
   include RedactorRails::Backend::CarrierWave
 
   # storage :fog
-  storage :file
+  storage :fog
 
   def store_dir
     "system/redactor_assets/documents/#{model.id}"
